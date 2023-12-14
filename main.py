3472461fcd22a7fd67265e506958296c7140c49f
nguyencong1227 <nguyenchiconganh12@gmail.com>
2023-12-14 10:43:41 +0900

    fine tune PhoGPT

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ from tokenization import tokenize_data
 from transformers import AutoTokenizer
 from model_setup import setup_trainer
 from transformers import AutoTokenizer
+import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig, pipeline, LongformerTokenizer
+import logging
 
 def main():
-    model = prepare_model("vinai/PhoGPT-7B5-Instruct")
+     # Configurations
+    DEVICE_TYPE = "cuda" 
+    #if torch.cuda.is_available() else "cpu"
+    SHOW_SOURCES = True
+    logging.info(f"Running on: {DEVICE_TYPE}")
+    logging.info(f"Display Source Documents set to: {SHOW_SOURCES}")
+    
+    # Model configurations
+    model_id = "vinai/PhoGPT-7B5-Instruct"
+    model_basename = "llama-2-7b-chat.ggmlv3.q4_0.bin"
+    model = prepare_model(device_type=DEVICE_TYPE, model_id=model_id, model_basename=None)
     print_trainable_parameters(model)
     data = load_and_process_data()
-    tokenizer = AutoTokenizer.from_pretrained("allenai/longformer-base-4096")
+    tokenizer = LongformerTokenizer.from_pretrained("allenai/longformer-base-4096")
     processed_data = tokenize_data(data, tokenizer)
     print(processed_data['train']["prediction"][:5])
 
--- model.py
+++ model.py
@@ -2,16 +2,79 @@ import torch
 import torch.nn as nn
 from transformers import AutoModelForCausalLM
 from peft import LoraConfig, get_peft_model
+import logging
+import torch
+from huggingface_hub import hf_hub_download
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig, pipeline, LlamaTokenizer
+from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
+from transformers import LlamaForCausalLM, LlamaTokenizer
+from langchain.llms import LlamaCpp
+from langchain.chains import LLMChain
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.llms import HuggingFacePipeline
+from tqdm import tqdm
 
-def prepare_model(model_path):
-    model = AutoModelForCausalLM.from_pretrained(
-        model_path,
-        load_in_8bit=True,
-        device_map='auto',
-        torch_dtype=torch.float16,
-        low_cpu_mem_usage=True,
-        trust_remote_code=True,
-    )
+def prepare_model(device_type, model_id, model_basename=None):
+    logging.info(f"Loading Model: {model_id}, on: {device_type}")
+    logging.info("This action can take a few minutes!")
+
+    if model_basename is not None:
+        if ".ggml" in model_basename:
+            logging.info("Using Llamacpp for GGML quantized models")
+            model_path = hf_hub_download(repo_id=model_id, filename=model_basename)
+            max_ctx_size = 4096
+            kwargs = {
+                "model_path": model_path,
+                "n_ctx": max_ctx_size,
+                "max_tokens": max_ctx_size,
+            }
+            if device_type.lower() == "mps":
+                kwargs["n_gpu_layers"] = 1000
+            if device_type.lower() == "cuda":
+                kwargs["n_gpu_layers"] = 1000
+                kwargs["n_batch"] = max_ctx_size
+            return LlamaCpp(**kwargs)
+        else:
+            logging.info("Using AutoGPTQForCausalLM for quantized models")
+
+            if ".safetensors" in model_basename:
+                # Remove the ".safetensors" ending if present
+                model_basename = model_basename.replace(".safetensors", "")
+
+            tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
+            logging.info("Tokenizer loaded")
+
+            model = AutoGPTQForCausalLM.from_quantized(
+                model_id,
+                model_basename=model_basename,
+                use_safetensors=True,
+                trust_remote_code=True,
+                device="cuda:0",
+                use_triton=False,
+                quantize_config=None,
+            )
+    elif (
+        device_type.lower() == "cuda"
+    ):
+        logging.info("Using AutoModelForCausalLM for full models")
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        logging.info("Tokenizer loaded")
+
+        model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            device_map="auto",
+            torch_dtype=torch.float16,
+            low_cpu_mem_usage=True,
+            trust_remote_code=True,
+            # max_memory={0: "15GB"} # Uncomment this line with you encounter CUDA out of memory errors
+        )
+        model.tie_weights()
+    else:
+        logging.info("Using LlamaTokenizer")
+        tokenizer = LlamaTokenizer.from_pretrained(model_id)
+        model = LlamaForCausalLM.from_pretrained(model_id)
+
+    generation_config = GenerationConfig.from_pretrained(model_id)
 
     for param in model.parameters():
         param.requires_grad = False  # freeze the model - train adapters later
